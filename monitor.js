93c6fe51f6e1418c2b856d7363a2ad0fa9497abf
IlyasN1 <149715008+IlyasN1@users.noreply.github.com>
2024-02-13 18:03:58 +0100

    Add files via upload

---
+++ monitor.js
@@ -0,0 +1,13 @@
+// Получите текущую ориентацию экрана
+var screenOrientation = window.screen.orientation;
+
+// Доступ к свойствам ориентации
+var angle = screenOrientation.angle;
+var orientation = screenOrientation.type;
+var onscreen = screenOrientation.onscreen;
+var portrait = screenOrientation.portrait;
+
+console.log("Ориентация экрана: " + orientation);
+console.log("Угол поворота: " + angle);
+console.log("Экран в портретной ориентации: " + portrait);
+console.log("Элемент на экране: " + onscreen);
