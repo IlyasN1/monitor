85f23b4a60d793a3ea9e88596999f02887e959a9
IlyasN1 <149715008+IlyasN1@users.noreply.github.com>
2024-05-28 17:04:04 +0200

    Update monitor.js

--- monitor.js
+++ monitor.js
@@ -8,6 +8,6 @@ var onscreen = screenOrientation.onscreen;
 var portrait = screenOrientation.portrait;
 
 console.log("Ориентация экрана: " + orientation);
-console.log("Угол поворота: " + angle);
+console.log("Угол поворота: " + angle);//Этот код возможно будет не нужен
 console.log("Экран в портретной ориентации: " + portrait);
 console.log("Элемент на экране: " + onscreen);
